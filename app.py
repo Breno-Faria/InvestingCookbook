422f524d7535c9ec2c4a9eafd6e3fc6b96b74ae2
felixpoirier1 <felixpoirier2020@gmail.com>
2023-01-21 17:01:49 -0500

    standard commit

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__, static_folder='static')
 @app.route("/")
 def home():
     context = {'title': "finance cookbook"}
+    return render_template("index.html", context=context)
+
+@app.route("/tutorial-compounding")
+def home():
+    context = {'title': "finance cookbook"}
     return render_template("compounding.html", context=context)
 
 
