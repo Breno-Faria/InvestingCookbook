8a78289bee7b70fba12c135603f30c05a5782cee
Breno Carneiro de Faria <breno@brenos-air.wireless.concordia.ca>
2023-01-21 17:19:27 -0500

    standard commit

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def home():
     context = {'title': "finance cookbook"}
     return render_template("index.html", context=context)
 
+@app.route("/tutorial-diversification")
+def diversify():
+    return render_template("diversification.html")
+
 @app.route("/tutorial-compounding")
-def compounding():
+def compound():
     context = {'title': "finance cookbook"}
     return render_template("compounding.html", context=context)
 
