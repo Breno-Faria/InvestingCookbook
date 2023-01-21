57fe792cbe92b1097f7bfa9e6eed66c15f9d96a0
felixpoirier1 <felixpoirier2020@gmail.com>
2023-01-21 17:42:48 -0500

    standard commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_sock import Sock
 
 app = Flask(__name__, static_folder='static')
+sock = Sock(app)
 
 @app.route("/")
 def home():
@@ -13,10 +15,15 @@ def diversify():
 
 @app.route("/tutorial-compounding")
 def compound():
+    data = [1,2, 4, 3, 4, 2]
     context = {'title': "finance cookbook"}
-    return render_template("compounding.html", context=context)
+    return render_template("compounding.html", context=context, data=data)
 
-
+@sock.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
 
 
 
